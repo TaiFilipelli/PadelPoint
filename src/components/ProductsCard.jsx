cf8a085d127413afa195dd4ba6a691c4e8571530
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-13 00:31:51 -0300

    ProductsCard already constructed. Can't modify it anymore till API is ready

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 
 const ProductsCard = ({productName, urlImg, price}) => {
     const [isLoading, setIsLoading] = useState[true];
+    
   return (
-    <section className='flex items-center justify-center'>
+    <section className='flex items-center justify-center p-6'>
         <h1 className='text-xl'>{productName}</h1>
         <img src={urlImg} alt="Product image" />
         <p className='text-lg'>{price}</p>
