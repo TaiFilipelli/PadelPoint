d6e3f9ee929a64b61ecab530b08ac0b9e6cb0f3b
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-13 18:16:41 -0300

    added brand logo to the navbar and some startups from what's coming once the API is finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,8 @@ import { Storefront, UsersThree, User, Moon, Sun } from 'phosphor-react'
  * @param {Object} props - Las propiedades del componente.
  * @param {boolean} props.isLogged - Estado que determina si el usuario está logeado o no.
  * @param {function} props.setIsLogged  - Función que cambia el estado de logeado de un usuario.
+ * @param {boolean} props.isDarkMode - Estado que determina si la página esta en modo oscuro
+ * @param {function} props.onToggleDarkMode - Evento que maneja cuando se togglea el modo oscuro desde el botón
  * @returns {JSX.Element} El componente NavBar
  */
 const Navbar = ({isLogged, setIsLogged, isDarkMode, onToggleDarkMode}) => {
@@ -16,11 +18,11 @@ const Navbar = ({isLogged, setIsLogged, isDarkMode, onToggleDarkMode}) => {
     setIsLogged(!isLogged);
   }
   return (
-    <nav className='bg-transparent flex flex-row p-4 w-full h-20 font-poppinsRegular'>
+    <nav className='bg-transparent flex flex-row p-4 w-full h-24 font-poppinsRegular'>
         <div className='container mx-auto flex justify-between gap-40 items-center text-black dark:text-white'>
-            <div className='text-black dark:text-white text-xl'>
+            <div /*className='dark:bg-white dark:rounded-full dark:h-[11rem]' PREGUNTAR A LOS PIBES QUÉ PREFIEREN*/>
                 {/* <img src="" alt="PadelPoint logo" /> */}
-                <Link to='/'>LOGO</Link>
+                <Link to='/'><img src="/public/LogoPadelPoint.png" alt="Logo Padel Point" className=' invert-on-dark h-[10rem] mb-2' /></Link>
             </div>
             <div className='space-x-4 flex gap-8'>
                 <Link to='/products' className='flex flex-row text-xl'>Productos <Storefront size={30} className='ml-2'/></Link>
@@ -29,6 +31,14 @@ const Navbar = ({isLogged, setIsLogged, isDarkMode, onToggleDarkMode}) => {
                 <button onClick={onToggleDarkMode}>{isDarkMode? <Moon size={30} className='ml-2'/> : <Sun size={30} className='ml-2'/>}</button>
             </div> 
         </div>
+        <style jsx='true'>{`
+          .invert-on-dark {
+            filter: invert(0);
+          }
+          .dark .invert-on-dark {
+            filter: invert(1);
+          }
+        `}</style>
     </nav>
   )
 }
---
+++ src/utils/services.js
@@ -0,0 +1,14 @@
+import { useAuthStore } from "../store/auth";
+
+export const verifyResponse = (response)=>{
+    if(!response.ok){
+        throw new Error(response.statusText);
+    }
+
+    return response.json();
+};
+
+export const getAuthHeader = ()=>{
+    const {token} =useAuthStore.getState();
+    return `Bearer ${token}`;
+}
