384d3669c39d935876d35931b3e9bb8d337c0103
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-12 21:34:20 -0300

    isLogged state added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Error404 from './components/pages/404Page.jsx';
 import AboutUs from './components/pages/AboutUs.jsx';
 import Navbar from './components/Navbar.jsx';
 import Product from './components/pages/Product.jsx';
+import { useState } from 'react';
 
 function App() {
+
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
     <main className='h-full w-full bg-no-repeat'>
-      <Navbar/>
+      <Navbar isLogged={isLogged} setIsLogged={setIsLogged}/>
       <Switch>
         <Route path='/' component={Inicio}/>
         <Route path='/products' component={Productos}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Link } from 'wouter'
-import { Storefront, UsersThree } from 'phosphor-react'
+import { Storefront, UsersThree, User } from 'phosphor-react'
 
-const Navbar = () => {
+const Navbar = ({isLogged, setIsLogged}) => {
+
+  const handleLoginButton=()=>{
+    setIsLogged(!isLogged);
+  }
   return (
     <nav className='bg-transparent flex flex-row p-4 w-full h-20'>
         <div className='container mx-auto flex justify-between gap-40 items-center text-white'>
@@ -10,11 +14,11 @@ const Navbar = () => {
                 {/* <img src="" alt="PadelPoint logo" /> */}
                 <Link to='/'>LOGO</Link>
             </div>
-
             <div className='space-x-4 flex gap-8'>
                 <Link to='/products' className='flex flex-row text-xl'>Productos <Storefront size={35} className='ml-2'/></Link>
                 <Link to='/about' className='flex flex-row text-xl'>About us <UsersThree size={35} className='ml-2'/></Link>
-            </div>
+                <button onClick={handleLoginButton} className='flex flex-row text-xl'>{ isLogged? 'Usuario' : 'Invitado' }<User size={25} className='ml-2'/></button>
+            </div> 
         </div>
     </nav>
   )
