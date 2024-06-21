05404eea8019fe739ba9d997eab811718a112edc
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-21 14:43:56 -0300

    new conversor to pesos argentinos made by Lau, implementing it right now

---
+++ src/components/ConversorButton.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export const ConversorButton = ({ valueToConvert }) => {
+  /* OPTO POR PONER TODA LA CONFIGURACION ACÁ, SI CREES QUE ES MAS UTIL MUDAR TODA LA LOGICA A UNA CARPETA 'SERVICES' HACELO */
+ /*  HOLA LAU, NO. LO DEJO ACÁ, ALTA PAJA PASARLO A UN SERVICES XD. ATTE: EL GOAT(ai)*/  
+
+  const [dolarBlueValue, setDolarBlueValue] = useState(null);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dolarapi.com/v1/dolares/blue")
+      .then((response) => response.json())
+      .then((data) => {
+        const { venta } = data;
+        setDolarBlueValue(Number(venta));
+      });
+  }, []);
+
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+  };
+
+  return (
+    <section className="relative w-12 h-12 rounded-full">
+      <button
+        className="bg-white shadow-sm h-full w-full text-2xl rounded-full"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+      >
+        🧉
+      </button>
+      <article
+        className={`${
+          isHovered ? "opacity-100" : "opacity-0"
+        } rounded-lg bg-white shadow text-center absolute -bottom-22 -right-52 h-max w-52 flex flex-col transition-all`}
+      >
+        <header className="bg-blue-400 h-4 rounded-tl-lg rounded-tr-lg"></header>
+        <section className="rounded-bl-lg rounded-br-lg flex flex-col p-2">
+          <span className="font-bold text-lg">
+            AR$
+            {Number(dolarBlueValue * valueToConvert).toFixed(2)}
+          </span>
+          <span className="text-xs">Dolar blue hoy: AR${dolarBlueValue}</span>
+        </section>
+      </article>
+    </section>
+  );
+};
