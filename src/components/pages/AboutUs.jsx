fbe76514c3793524e905a6604821a2b968fa9f55
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-25 20:08:26 -0300

    better width!

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -5,29 +5,29 @@ const AboutUs = () => {
     <section className='about-section justify-center items-center text-center'>
       <div className='about-container'>
       <h1 className='text-4xl font-poppinsBold mb-8 mt-4'>Esto es PadelPoint</h1>
-      <h3 className='text-xl font-poppinsRegular w-[50%] mx-auto'>¡Hola! Soy Ignacio Merlo, fundador de PadelPoint, y estoy encantado de darte la bienvenida a 
+      <h3 className='text-xl font-poppinsRegular w-[70%] mx-auto'>¡Hola! Soy Ignacio Merlo, fundador de PadelPoint, y estoy encantado de darte la bienvenida a 
         nuestra tienda. Tengo 23 años, soy jugador y profesor de pádel, y tengo una verdadera pasión por este deporte. Inicié PadelPoint hace 
         tres meses con el objetivo de ofrecer un lugar de confianza donde los jugadores de pádel de toda Argentina puedan encontrar todo lo que necesitan</h3>
       <h3 className='text-3xl font-poppinsBold mb-8 mt-10'>Nuestra Inspiración</h3>
-      <h2 className='text-xl font-poppinsRegular w-[50%] mx-auto'>La idea de crear PadelPoint nació de mi propio amor por las paletas de pádel y la necesidad que vi 
+      <h2 className='text-xl font-poppinsRegular w-[70%] mx-auto'>La idea de crear PadelPoint nació de mi propio amor por las paletas de pádel y la necesidad que vi 
         en nuestra comunidad de tener un proveedor confiable y especializado. Como jugador, sé lo importante que es contar con el equipo adecuado, y como 
         profesor, entiendo las necesidades de los jugadores a diferentes niveles. Juego hace 3 años, compitiendo en la 3ra categoría, y he sido número 1 local 
         en mi categoría en múltiples ocasiones. Además, llevo 1.5 años dando clases, ayudando a otros a mejorar su juego y disfrutar más del pádel.</h2>
       <h3 className='text-3xl font-poppinsBold mb-8 mt-10'>Nuestra Misión</h3>
-      <h2 className='text-xl font-poppinsRegular w-[50%] mx-auto'>En PadelPoint, nuestra misión es simple pero ambiciosa: queremos llegar a todos los jugadores de 
+      <h2 className='text-xl font-poppinsRegular w-[70%] mx-auto'>En PadelPoint, nuestra misión es simple pero ambiciosa: queremos llegar a todos los jugadores de 
         pádel del país y equiparlos con productos de la mejor calidad al mejor precio posible. Ofrecemos una amplia gama de paletas, bolsos y zapatillas, 
         seleccionados cuidadosamente para satisfacer tanto a principiantes como a jugadores avanzados.</h2>
       <h3 className='text-3xl font-poppinsBold mb-8 mt-10'>Lo que nos hace únicos</h3>
-      <h2 className='text-xl font-poppinsRegular w-[50%] mx-auto'>Lo que nos distingue de otras tiendas de pádel es nuestro compromiso con la calidad y la 
+      <h2 className='text-xl font-poppinsRegular w-[70%] mx-auto'>Lo que nos distingue de otras tiendas de pádel es nuestro compromiso con la calidad y la 
         satisfacción del cliente. Nos enorgullece recibir una respuesta tan positiva de nuestros clientes; más de 50 personas han compartido sus 
         compras en Instagram para ayudarnos a crecer. Nos esforzamos por crear una comunidad de jugadores apasionados que confíen en nosotros para
         sus necesidades de equipo.</h2>
       <h3 className='text-3xl font-poppinsBold mb-8 mt-10'>Nuestros planes futuros</h3>
-      <h2 className='text-xl font-poppinsRegular w-[50%] mx-auto'>En PadelPoint, siempre estamos pensando en el futuro. Uno de nuestros grandes proyectos 
+      <h2 className='text-xl font-poppinsRegular w-[70%] mx-auto'>En PadelPoint, siempre estamos pensando en el futuro. Uno de nuestros grandes proyectos 
         es sponsorear a jugadores profesionales para ayudar a crecer la marca y apoyar el talento emergente en el pádel. Creemos que respaldar a los 
         jugadores no solo promueve nuestro negocio, sino que también fortalece la comunidad de pádel en general.</h2>
       <h3 className='text-3xl font-poppinsBold mb-8 mt-10'>Únete a nuestra comunidad!</h3>
-      <h2 className='text-xl font-poppinsRegular w-[50%] mx-auto'>Te invitamos a explorar nuestra tienda, conocer nuestros productos y unirte a la comunidad
+      <h2 className='text-xl font-poppinsRegular w-[70%] mx-auto'>Te invitamos a explorar nuestra tienda, conocer nuestros productos y unirte a la comunidad
         PadelPoint. Ya sea que estés buscando tu próxima paleta, un nuevo par de zapatillas, o simplemente quieras conectarte con otros apasionados del pádel, 
         estamos aquí para ayudarte. ¡Gracias por visitarnos y esperamos verte pronto en la cancha!</h2>
       </div>
