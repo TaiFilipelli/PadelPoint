96cf0ed0915a0882e21f436071dbdb7d6c799c20
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-25 20:24:43 -0300

    stock pictures added to personalize Inicio and About

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -4,6 +4,7 @@ const Inicio = () => {
   return (
     <section className='flex justify-center flex-col items-center'>
       <h1 className='text-5xl mt-5 font-poppinsBold'>PadelPoint</h1>
+      {/* <img src="/PadelStock1.png" alt="Paletas Stock 1" className='rounded-md w-full' /> Por algún motivo que desconozo, siento que se ve horrible */} 
       <h3 className='font-poppinsRegular text-lg w-2/4 mt-10 max-[350px]:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, 
       maxime dolores modi sed temporibus id? Officia impedit at atque, nam dolore laudantium perspiciatis amet nisi 
       distinctio est molestias iste quas!</h3>
