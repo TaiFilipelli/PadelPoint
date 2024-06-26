5ce5bfff347935df826be0fbc08d929aa42e1347
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-26 12:51:45 -0300

    schemas properly added and huge improve on input handle

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,44 +1,79 @@
 import { Label } from 'keep-react';
 import { useState } from 'react';
 import { Link } from 'wouter';
-const Register = () => {
+import { registerSchema } from '../../../schemas/Register'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
-    const [name, setName] = useState('');
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const handleSubmit = (e) =>{
-      e.preventDefault();
+const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(registerSchema),
+    });
+    const onSubmit = (data) => {
+        console.log(data);
+        // Aquí puedes manejar el envío del formulario, por ejemplo, hacer una petición a la API
     }
 
-  return (
-    <section className='flex flex-wrap absolute top-0 left-0 w-full h-full z-50 bg-custom-dark-bg'>
-      <div className='register-bg w-1/2 bg-no-repeat'/>
-      <section className='flex text-left items-left justify-center flex-col ml-3'>
-        <h1 className='font-poppinsBlack text-5xl'>Únete a la familia PadelPoint hoy!</h1>
-        <form onSubmit={handleSubmit}>
-          <fieldset className='p-2 mt-4'>
-            <Label htmlFor='name' className='mr-4 font-poppinsMedium text-white text-xl'>Nombre y apellido</Label>
-            <input type="text" id='name' required className='rounded-md w-1/3 bg-white text-black p-1 font-poppinsRegular' placeholder='John Doe' value={name} />
-          </fieldset>
-          <fieldset className='p-2 mt-2'>
-            <Label htmlFor='email' className='mr-4 font-poppinsMedium text-white text-xl'>Correo electrónico</Label>
-            <input type="email" id='email' required className='rounded-md w-1/3 bg-white text-black p-1 font-poppinsRegular' placeholder='example@mail.com' value={email} />
-          </fieldset>
-          <fieldset className='p-2 mt-4'>
-            <Label htmlFor='password' className='mr-4 font-poppinsMedium text-white text-xl'>Contraseña</Label>
-            <input type="password" id='password' required className='rounded-md w-1/3 bg-white text-black p-1 font-poppinsRegular' />
-          </fieldset>
-          <fieldset className='p-2 mt-2'>
-            <Label htmlFor='confpassword' className='mr-4 font-poppinsMedium text-white text-xl'>Confirmar contraseña</Label>
-            <input type="confpassword" id='confpassword' required className='rounded-md w-1/3 bg-white text-black p-1 font-poppinsRegular' />
-          </fieldset>
-          <button type='submit' onSubmit={handleSubmit} className='font-poppinsMedium p-1 bg-black'>Registrarme</button>
-        </form>
-        <Link to='/' className='hover:underline ml-2'>Volver atrás</Link>
-      </section>
-    </section>
-
-  )
+    return (
+        <section className='flex flex-wrap absolute top-0 left-0 w-full h-full z-50 bg-custom-dark-bg'>
+            <div className='register-bg w-1/2 bg-no-repeat'/>
+            <section className='flex flex-col items-start justify-center p-6 w-full sm:w-1/2'>
+                <h1 className='font-poppinsBlack text-5xl mb-6'>¡Únete a la familia PadelPoint hoy!</h1>
+                <form onSubmit={handleSubmit(onSubmit)} className='w-full'>
+                    <fieldset className='p-2 mt-4'>
+                        <Label htmlFor='userName' className='block font-poppinsMedium text-white text-xl mb-1'>Nombre y apellido</Label>
+                        <input 
+                            type="text" 
+                            id='userName' 
+                            className='rounded-md w-1/2 bg-white text-black p-2 font-poppinsRegular' 
+                            placeholder='John Doe' 
+                            {...register('userName')}
+                        />
+                        {errors.userName && <span className='text-red-500 font-poppinsBlack ml-6'>{errors.userName.message}</span>}
+                    </fieldset>
+                    <fieldset className='p-2 mt-2'>
+                        <Label htmlFor='email' className='block font-poppinsMedium text-white text-xl mb-1'>Correo electrónico</Label>
+                        <input 
+                            type="email" 
+                            id='email' 
+                            className='rounded-md w-1/2 bg-white text-black p-2 font-poppinsRegular' 
+                            placeholder='example@mail.com' 
+                            {...register('email')}
+                        />
+                        {errors.email && <span className='text-red-500 font-poppinsBlack ml-6'>{errors.email.message}</span>}
+                    </fieldset>
+                    <fieldset className='p-2 mt-2'>
+                        <Label htmlFor='password' className='block font-poppinsMedium text-white text-xl mb-1'>Contraseña</Label>
+                        <input 
+                            type="password" 
+                            id='password' 
+                            className='rounded-md w-1/2 bg-white text-black p-2 font-poppinsRegular' 
+                            placeholder='Contraseña...'
+                            {...register('password')}
+                        />
+                        {errors.password && <span className='text-red-500 font-poppinsBlack ml-6'>{errors.password.message}</span>}
+                    </fieldset>
+                    <fieldset className='p-2 mt-2'>
+                        <Label htmlFor='confirmPassword' className='block font-poppinsMedium text-white text-xl mb-1'>Confirmar contraseña</Label>
+                        <input 
+                            type="password" 
+                            id='confirmPassword' 
+                            className='rounded-md w-1/2 bg-white text-black p-2 font-poppinsRegular' 
+                            placeholder='Confirmar contraseña...'
+                            {...register('confirmPassword')}
+                        />
+                        {errors.confirmPassword && <span className='text-red-500'>{errors.confirmPassword.message}</span>}
+                    </fieldset>
+                    <button 
+                        type='submit' 
+                        className='font-poppinsMedium p-3 bg-black text-white rounded-md mt-4 ml-2 w-1/3'>
+                        Registrarme
+                    </button>
+                </form>
+                <Link to='/' className='hover:underline text-white mt-4 ml-2'>Volver atrás</Link>
+            </section>
+        </section>
+    );
 }
 
-export default Register
+export default Register;
