5e55731f84e7e0690298b28852e10748c6f3a48b
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-27 19:03:22 -0300

    404 page ready

--- src/components/pages/404Page.jsx
+++ src/components/pages/404Page.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'wouter'
 
 const Error404 = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center text-center flex-col items-center text-black dark:text-white'>
+      <h1 className='font-poppinsBlack text-5xl mt-16 mb-8'>ERROR 404: NOT FOUND</h1>
+      <img
+          src="https://staticmania.cdn.prismic.io/staticmania/ed90f683-c1df-4bad-afa4-65ce4c65287e_Property+1%3DSpaceship_+Property+2%3DMd.svg"
+          height={234}
+          width={350}
+          alt="404"
+          className='rounded-md bg-black dark:bg-transparent'
+        />
+        <h3 className='font-poppinsRegular text-2xl mt-10'>Ups! Parece que tanta calidad te llevó a un lugar donde no hay nada! No se encontró la pagina.</h3>
+        <Link to='/' className='font-poppinsLight text-xl hover:underline hover:text-red-600 transition mt-10'>Volver al inicio</Link>
     </div>
   )
 }
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -5,6 +5,7 @@ const Inicio = () => {
   return (
     <section className='flex justify-center flex-col items-center text-center'>
       <h1 className='font-poppinsBold text-4xl w-2/4 mt-10 max-[350px]:text-base'>Bienvenido a PadelPoint!</h1>
+      <h3 className='font-poppinsMedium text-2xl w-1/2 mt-5 max-[350px]:text-sm'>Encontrá </h3>
     </section>
   )
 }
