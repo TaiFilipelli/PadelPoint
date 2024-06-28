c097add783e64212e9bb7862f8598be63e5ef25f
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-27 23:01:40 -0300

    funny adition to the FuturePayment page! Still working on it

--- src/components/pages/FuturePayment.jsx
+++ src/components/pages/FuturePayment.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import 'animate.css';
 const FuturePayment = () => {
   return (
-    <section className='absolute z-50'>
-        <h1>MÉTODO DE PAGO EN CONSTRUCCIÓN</h1>
-        <h3>El presupuesto dió hasta acá, en breves pondremos un sistema de pago funcional!</h3>
-        <a href="/">Volver al inicio</a>
+    <section className='absolute z-50 items-center justify-center text-center w-full mt-10'>
+        <h1 className='font-poppinsBlack text-6xl animate__hinge animate__slower'>MÉTODO DE PAGO EN CONSTRUCCIÓN</h1>
+        <h3 className='font-poppinsMedium mt-5 text-2xl mb-5'>El presupuesto dió hasta acá, en breves pondremos un sistema de pago funcional!</h3>
+        <a href="/" className='text-red-600 font-poppinsBold text-xl hover:underline'>Volver al inicio</a>
     </section>
   )
 }
