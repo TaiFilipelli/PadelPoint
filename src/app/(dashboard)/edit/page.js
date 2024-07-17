67881dfe5b44f86fe19c223d34f573fa8a0bade5
TaiFilipelli <taielfilipelli@gmail.com>
2024-07-17 11:38:55 -0300

    created EditRacket page

---
+++ src/app/(dashboard)/edit/page.js
@@ -0,0 +1,7 @@
+export default function EditRacket() {
+    return (
+        <section>
+            <h1>Aquí editamos las paletas, habrá un método desde la API con su respectivo endpoint</h1>
+        </section>
+    );
+}
