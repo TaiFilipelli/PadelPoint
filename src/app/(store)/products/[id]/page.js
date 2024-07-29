5fc6f068d2098bc3ce567b775aebfa9e019e9970
TaiFilipelli <taielfilipelli@gmail.com>
2024-07-29 20:00:04 -0300

    renewed product detail page and fixed an unnecesary "w-screen" that made the page to have an horrible horizontal scroll in each one of the browsers i tried. Glad i fixed that.

--- src/app/(store)/products/[id]/page.js
+++ src/app/(store)/products/[id]/page.js
@@ -35,18 +35,30 @@ export default function ProductDetailPage() {
   }
 
   return (
-    <section className={`flex flex-col justify-center text-center items-center p-20 ${pop.className} p-10`}>
-      <h1 className='text-4xl'>{product.name}</h1>
-      <div className="h-[40rem]">
-        <img src={product.image} alt={product.name} className="h-full" />
-      </div>
-      <div className="text-left w-1/3">
-        <p className="text-xl">Marca: {product.brand.name}</p>
-        <p className="text-xl">Precio (USD): ${product.price}</p>
-        <p className="text-xl">Más info proximamente...</p>
-        <Button className="rounded-lg py-4 mt-4 px-6 text-xl bg-transparent border-2 hover:bg-gradient-to-b from-pink-600 to-yellow-400 ease-in-out transition-all hover:border-black" variant="light">Comprar</Button>
-      </div>
-      <Link href='/products' className="mt-5 text-lg hover:underline hover:text-red-600 transition-colors">Volver a ver los productos</Link>
+    <section className={`flex justify-center items-center p-16 ${pop.className}`}>
+      <section className="flex flex-wrap">
+        <div className="flex flex-wrap p-2">
+          <div className="block w-24 mr-2">
+            <img src={product.image} alt={product.name} className="border-1 rounded-xl"/>
+            <img src={product.image} alt={product.name} className="border-1 rounded-xl"/>
+            <img src={product.image} alt={product.name} className="border-1 rounded-xl"/>
+            <img src={product.image} alt={product.name} className="border-1 rounded-xl"/>
+          </div>
+          <div className="h-[30rem]">
+            <img src={product.image} alt={product.name} className="h-full border-1 rounded-xl" />
+          </div>
+        </div>
+        <div className="text-left flex flex-col p-2">
+          <h1 className='text-4xl mb-10'>{product.name}</h1>
+          <p className="text-3xl mb-10">${product.price}</p>
+          <p className="text-xl">Marca: {product.brand.name}</p>
+          <p className="text-xl">Más info proximamente...</p>
+          <Button className="rounded-lg py-4 mt-4 w-1/2 px-4 text-xl bg-transparent border-2 hover:bg-gradient-to-b from-pink-600 to-yellow-400 ease-in-out transition-all hover:border-black" variant="light">Comprar</Button>
+          <Link href='/products' className="mt-5 text-lg hover:underline hover:text-red-600 transition-colors">Volver a ver los productos</Link>
+        </div>
+      </section>
+      
+      
     </section>
   );
 }
