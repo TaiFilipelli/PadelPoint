cf6a9a2592a3d7518cfbd350b8bd990e828d54a6
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-05 13:12:25 -0300

    limitedName applied: products do not longer show the full name, providing better handling of responsive later

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -2,12 +2,10 @@ import Link from "next/link";
 import { Skeleton } from "@nextui-org/react";
 
 const ProductsCard = ({name, image, brand, price, idProducto, isLoading}) => {
-    const maxlength = 15;
-    // const limitedName = name.length > maxlength ? name.substring(0,maxlength) + '...' : name;
 
     if (isLoading) {
       return (
-        <section className='bg-default-100 text-black w-1/4 flex flex-col justify-center items-center rounded-lg text-center border-2 border-gray-200 shadow-xl p-4'>
+        <section className='bg-default-100 text-black w-1/4 flex flex-col justify-center items-center rounded-lg text-center border-2 border-gray-200 shadow-xl p-4 my-4'>
           <Skeleton className='mt-2 w-3/4 rounded-md'>
             <div className="bg-secondary-50 h-7"></div>
           </Skeleton>
@@ -26,10 +24,11 @@ const ProductsCard = ({name, image, brand, price, idProducto, isLoading}) => {
         </section>
       );
     }
-
+  const maxlength = 20;
+  const limitedName = name.length > maxlength ? name.substring(0,maxlength) + '...' : name;
   return (
-    <section className='bg-white dark:bg-gray-700 text-black w-1/4 flex flex-col justify-center items-center rounded-lg text-center border-2 border-gray-200 shadow-xl p-4'>
-      <h1 className='mt-2 text-lg font-bold'>{name}</h1>
+    <section className='bg-white dark:bg-gray-700 text-black w-1/4 flex flex-col justify-center items-center rounded-lg text-center border-2 border-gray-200 shadow-xl p-4 my-4'>
+      <h1 className='mt-2 text-lg font-bold'>{limitedName}</h1>
       <img src={image} alt="Imagen paleta" className="h-[20rem]"/>
       <p className='font-semibold mt-2'>Marca: {brand}</p>
       <div className='w-full flex items-center justify-center mt-2'>
