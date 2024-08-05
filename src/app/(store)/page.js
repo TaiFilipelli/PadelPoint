41c61afb8650f51a3a53e83e6d17d98976eb0515
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-05 08:52:18 -0300

    first steps with new query params

--- src/app/(store)/page.js
+++ src/app/(store)/page.js
@@ -14,7 +14,6 @@ export default function Home() {
       <h1 className={`${pop.className} text-6xl`}>PadelPoint Oficial</h1>
       <MyCarousel/>
       <h2 className={`${pop.className} text-3xl mt-5`}>Productos destacados:</h2>
-      {/* (próximamente, con la inclusión de querys compuestas) */}
       <div className="flex flex-row justify-center gap-10 w-1/2 mt-10 mb-14">
         <ProductsCard isLoading={true}/>
         <ProductsCard isLoading={true}/>
