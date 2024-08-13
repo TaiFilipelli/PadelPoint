677fea278c2f8db3e29994f99ffa4f56c761895e
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-13 10:44:18 -0300

    Added an isLoading state an a little circular progress that shows to the user just a second. I did it because sometimes the username stays blank until the page set that space with the username stored on local storage

--- src/app/(store)/login/result/page.js
+++ src/app/(store)/login/result/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from "react";
-import { Input, Button } from "@nextui-org/react";
+import { Input, Button, CircularProgress } from "@nextui-org/react";
 import Link from "next/link";
 import { searchUserAuthenticated, updateOneUser} from "src/data/data";
 import { toast, ToastContainer, Slide } from "react-toastify";
@@ -12,6 +12,7 @@ export default function ResultLogin() {
     const [isNewUser, setIsNewUser] = useState(false);
     const [username, setUsername] = useState('');
     const [newUsername, setNewUsername] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
     const router = useRouter();
 
@@ -49,30 +50,37 @@ export default function ResultLogin() {
     }
 
     useEffect(()=>{
+        setTimeout(()=>{
+            setIsLoading(false);
+        }, 1000)
         checkLoggedUser();
     },[]);
 
     return (
         <section className="flex items-center justify-center text-center p-20 flex-col">
-            { isNewUser ?
-            <>
-            <section className="bg-gray-200 flex flex-col w-1/2 p-8 mt-10 rounded-lg text-left">
-                <h1 className="font-bold text-3xl mb-2">Una última cosa...</h1>
-                <h2 className="font-semibold text-xl mb-2">Bievenido a la familia PadelPoint! Como es su primera vez aquí, nos gustaría que ingrese un nombre de usuario por única vez
-                    para reconocerlo futuras veces!
-                </h2>
-                <Input type="text" labelPlacement="outside" label='Nombre de usuario nuevo' className="mb-2" value={newUsername} onChange={(e)=> setNewUsername(e.target.value)}/>
-                <Button className="bg-blue-500 rounded-lg p-4 w-1/4 text-white font-semibold text-xl" onClick={handleUsernameChange}>Unirme</Button>
-            </section>
-            <ToastContainer position="bottom-right" autoClose={2500} transition={Slide} theme="light" closeOnClick draggable/>
-            </>
-            :
-            <section className="flex flex-col items-center text-center p-10">
-                <h1 className="text-green-700 font-bold text-4xl mb-4">Bienvenido, {username}</h1>
-                <h2 className="font-semibold text-xl mb-8">Puede retornar a la tienda y comprar libremente</h2>
-                <Link href='/' className="hover:underline">Volver al inicio</Link>
-            </section>
-            }
+           {isLoading ? (
+                <CircularProgress size="lg"/>
+            ) : (
+                isNewUser ? (
+                    <>
+                        <section className="bg-gray-200 flex flex-col w-1/2 p-8 mt-10 rounded-lg text-left">
+                            <h1 className="font-bold text-3xl mb-2">Una última cosa...</h1>
+                            <h2 className="font-semibold text-xl mb-2">Bievenido a la familia PadelPoint! Como es su primera vez aquí, nos gustaría que ingrese un nombre de usuario por única vez
+                                para reconocerlo futuras veces!
+                            </h2>
+                            <Input type="text" labelPlacement="outside" label='Nombre de usuario nuevo' className="mb-2" value={newUsername} onChange={(e) => setNewUsername(e.target.value)} />
+                            <Button className="bg-blue-500 rounded-lg p-4 w-1/4 text-white font-semibold text-xl" onClick={handleUsernameChange}>Unirme</Button>
+                        </section>
+                        <ToastContainer position="bottom-right" autoClose={2500} transition={Slide} theme="light" closeOnClick draggable />
+                    </>
+                ) : (
+                    <section className="flex flex-col items-center text-center p-10">
+                        <h1 className="text-green-700 font-bold text-4xl mb-4">Bienvenido, {username}</h1>
+                        <h2 className="font-semibold text-xl mb-8">Puede retornar a la tienda y comprar libremente</h2>
+                        <Link href='/' className="hover:underline">Volver al inicio</Link>
+                    </section>
+                )
+            )}
         </section>
     );
 }
