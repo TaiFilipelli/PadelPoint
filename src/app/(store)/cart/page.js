cfd729102fa97cd938f0e86fffa48f73cd280a57
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-15 09:44:21 -0300

    Mega cart update: fixed issue that wasn't allowing the page to re-render the content if the cart was empty and added the posibility to increase and decrease the amount of products you want to buy.

--- src/app/(store)/cart/page.js
+++ src/app/(store)/cart/page.js
@@ -3,38 +3,54 @@ import { Poppins } from "next/font/google";
 import { useCartStore } from "src/data/useCartStore";
 import { useState, useEffect } from "react";
 import { getOneProductById } from "src/data/data";
-import { SmileySad, Trash } from "@phosphor-icons/react";
+import { SmileySad, Trash, LockKey, Plus, Minus } from "@phosphor-icons/react";
 import { Divider, Button } from "@nextui-org/react";
 
-const pop = Poppins({subsets:['latin'],weight:['700','400']})
+const pop = Poppins({subsets:['latin'],weight:['700','400']});
+
 export default function Carrito() {
     const cart = useCartStore((state) => state.cart);
     const removeFromCart = useCartStore((state) => state.removeFromCart);
+    const updateCartItem = useCartStore((state) => state.updateCartItem);
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
         const fetchProducts = async () => {
           if (cart.length > 0) {
-            const productsData = await Promise.all(cart.map(async id => {
-              const res = await getOneProductById(id);
-              return res;
+            const productsData = await Promise.all(cart.map(async item => {
+              const res = await getOneProductById(item.id);
+              return { ...res, cantidad: item.cantidad };
             }));
             setProducts(productsData);
           }
+          else{
+            setProducts([]);
+          }
         };
-    
         fetchProducts();
       }, [cart]);
+
+    const handleIncrease = (idProducto, cantidad) => {
+        const newCantidad = cantidad + 1;
+        updateCartItem(idProducto, newCantidad);
+    };
+
+    const handleDecrease = (idProducto, cantidad) => {
+        if (cantidad > 1) {
+            const newCantidad = cantidad - 1;
+            updateCartItem(idProducto, newCantidad);
+        }
+    };
 
     return (
         <section className="p-16 flex flex-col items-center text-center">
             <h1 className={`${pop.className} text-5xl font-bold mb-4`}>Carrito</h1>
-              {products.length < 1 ? (
+            {products.length < 1 ? (
                 <section className="flex flex-col my-10 items-center justify-center rounded-3xl p-11 border-1 border-black bg-default-100">
                     <h2 className={`${pop.className} font-semibold text-3xl m-2`}>Tu carrito está vacío</h2>
                     <SmileySad size={80}/>
                 </section>
-              ) : (
+            ) : (
                 products.map(product => (
                   <section key={product.id} className={`${pop.className} flex items-center justify-between p-4 my-4 w-2/5 bg-default-300 rounded-lg`}>
                     <div className="flex items-center">
@@ -43,20 +59,23 @@ export default function Carrito() {
                         <h2 className='text-lg font-semibold'>{product.name}</h2>
                         <p className='font-normal'>Marca: {product.brand.name}</p>
                         <p className='font-normal'>Precio: US${product.price}</p>
+                        <div className="flex items-center gap-2 my-2">
+                            <Button onClick={() => handleDecrease(product.id, product.cantidad)} className="bg-transparent text-black px-2"><Minus weight="regular" size={20}/></Button>
+                            <p className="font-medium">{product.cantidad}</p>
+                            <Button onClick={() => handleIncrease(product.id, product.cantidad)} className="bg-transparent text-black px-2"><Plus/></Button>
+                        </div>
                       </div>
-                   </div>
-                    <button 
-                      onClick={() => removeFromCart(product.id)} 
-                      className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-400 transition-all font-medium">
-                      <Trash size={25}/>
-                    </button>
+                    </div>
+                    <Button className="bg-red-500 text-white px-2" onClick={() => removeFromCart(product.id)}>
+                        <Trash size={24} />
+                    </Button>
                   </section>
-              ))
+                ))
             )}
             <Divider/>
-            <div className={`${pop.className} flex flex-row text-left justify-between items-center w-2/5 mt-6`}>
-              <h1 className="font-medium text-xl">Total a pagar:</h1>
-              <Button isDisabled className='text-white font-medium text-xl bg-red-600'>Finalizar pago</Button>
+            <div className="flex flex-row justify-between w-1/2 my-4">
+              <h3 className="text-3xl font-semibold">Subtotal: $$$</h3>
+              <Button className="bg-red-600 text-white font-semibold text-xl px-6 py-6" startContent={<LockKey weight="duotone" size={25}/>} isDisabled>Finalizar compra</Button>
             </div>
         </section>
     );
--- src/data/useCartStore.js
+++ src/data/useCartStore.js
@@ -1,17 +1,35 @@
 import { create } from 'zustand';
 
+// Inicializamos el carrito creando uno.
 export const useCartStore = create((set) => ({
+  // Se crea el array 'Cart', nuestro carrito
   cart: JSON.parse(typeof window !== "undefined" && localStorage.getItem('cart')) || [],
-  
-  //Método para añadir al carrito y manejar los estados globales
+
+  //A continuación, los métodos que podremos usar para manipular el carrito:
+
+  // Primer método: añadir items al carrito. Si el producto existe, se devuelve con normalidad. 
   addToCart: (idProducto) => set((state) => {
-    const updatedCart = [...state.cart, idProducto];
+    const existingProductIndex = state.cart.findIndex(item => item.id === idProducto);
+
+    if (existingProductIndex >= 0) {
+      return state; 
+    }
+
+    const updatedCart = [...state.cart, { id: idProducto, cantidad: 1 }];
     localStorage.setItem('cart', JSON.stringify(updatedCart));
     return { cart: updatedCart };
   }),
 
   removeFromCart: (idProducto) => set((state) => {
-    const updatedCart = state.cart.filter(id => id !== idProducto);
+    const updatedCart = state.cart.filter(item => item.id !== idProducto);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+    return { cart: updatedCart };
+  }),
+
+  updateCartItem: (idProducto, cantidad) => set((state) => {
+    const updatedCart = state.cart.map(item => 
+      item.id === idProducto ? { ...item, cantidad } : item
+    );
     localStorage.setItem('cart', JSON.stringify(updatedCart));
     return { cart: updatedCart };
   }),
