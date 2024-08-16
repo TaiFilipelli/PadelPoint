8931d523b83a298738bdf732ae13d52fc6c228d2
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-16 08:59:42 -0300

    updated methods and info display, new API version allow's to display better and even more important information on product detail

--- src/app/(store)/products/[id]/page.js
+++ src/app/(store)/products/[id]/page.js
@@ -59,11 +59,9 @@ export default function ProductDetailPage() {
       <section className="flex flex-wrap my-6">
         <div className="flex flex-wrap p-2">
           <div className="block w-24 mr-2">
-            {/* Aquí mi idea es tener un método donde por cada imagen que tenga una paleta se despliegue aquí, futura implementación a conversar con Lau */}
-            <img src={product.image} alt={product.name} className="border-1 rounded-xl mb-1"/>
-            <img src={product.image} alt={product.name} className="border-1 rounded-xl mb-1"/>
-            <img src={product.image} alt={product.name} className="border-1 rounded-xl mb-1"/>
-            <img src={product.image} alt={product.name} className="border-1 rounded-xl"/>
+          {product.secondariesImages.map(image => (
+              <img key={image.id} src={image.url} alt={`Image ${image.id}`} className="border-1 rounded-xl mb-1" />
+           ))}
           </div>
           <div className="h-[35rem]">
             <img src={product.image} alt={product.name} className="h-full border-1 rounded-xl" />
@@ -73,7 +71,7 @@ export default function ProductDetailPage() {
           <h1 className='text-4xl mb-10'>{product.name}</h1>
           <p className="text-3xl mb-10">US${product.price}</p>
           <p className="text-xl">Marca: {product.brand.name}</p>
-          <p className="text-xl">Más info proximamente...</p>
+          {/* <p className="text-xl">Tipo: {product.type.name}</p> */}
           <Button className="rounded-lg py-4 mt-4 w-2/3 px-4 text-xl bg-transparent border-2 hover:bg-gradient-to-tr from-red-300 to-red-600 ease-in-out transition-all 
           hover:border-black hover:text-white" variant="light" onClick={()=>addToCart(product.id)}>Comprar</Button>
           <Link href='/products' className={`mt-5 text-lg hover:underline hover:text-red-600 transition-colors ${pop.className} font-normal`}>Volver a ver los productos</Link>
@@ -82,15 +80,7 @@ export default function ProductDetailPage() {
       <Divider/>
       <div className={`${pop.className} font-normal bg-default-200 rounded-lg flex text-left flex-col p-4 my-6`}>
         <h1 className={`text-4xl mb-6 mt-2 ${pop.className} font-semibold`}>Descripción del producto</h1>
-        <p className="text-lg">Acá iria una descripción del producto en cuestión. El caso ideal seria que la propia entidad Paleta tenga un campo varchar donde se proporcione una minúscula
-          descripción de cada paleta o, en su defecto, que haya algun campo "Attributes" donde yo pueda fetchear dichos atributos y armar una descripción decente y genérica para todas las
-          paletas del e-commerce, pero que varíen dichos atributos clave.
-        </p> <br/>
-        <p className="text-lg">
-          En caso no podamos contar con ello, simplemente pondría algun intento de "guía" para que los usuarios entiendan algunas cuestiones de la paleta. EJEMPLO: En caso "Tipo" sea algo
-          que especifiquemos en esta página, contamos las caracteristicas y principales diferencias entre las paletas de tipo Diamante, de tipo Redondo o Híbridas. Otra sección también 
-          puede ser info de envíos, de pagos, garantía u otras cuestiones del negocio.
-        </p>
+        <p className="text-lg">{product.description}</p>
       </div>
       <Divider/>
       <div className="flex flex-col items-center text-center my-6">
