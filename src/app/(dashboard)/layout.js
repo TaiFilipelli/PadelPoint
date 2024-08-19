37af605f56d6bcffdb9a10030d7329c764e1f43f
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-19 17:59:21 -0300

    comments to myself so i don't lose myself between so much unused code. Deleted middleware, was a method unnecesary, mostly because of the work that the API is already doing verifying the token

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -1,6 +1,8 @@
 import { Providers } from "../providers";
 import '../../../styles/globals.css';
 import { Montserrat, Roboto } from "next/font/google";
+// import { checkUserState } from "src/data/data"; 
+
 
 export const metadata = {
     title: 'Dashboard de Administrador',
@@ -9,7 +11,20 @@ export const metadata = {
 
 const mont = Montserrat({subsets:['latin'], weight:'600'});
 const robtop = Roboto({subsets:['greek'],weight:'300'});
-export default function DashboardLayout({children}) {
+export default async function DashboardLayout({children}) {
+
+  // const token = await checkUserState() U OTRO MÉTODO QUE BUSQUE LA TOKEN
+
+  // if (!token) {
+  //   redirect('/login');
+  // }
+
+  // const user = await verifyToken(token); U OTRO MÉTODO PARA VERIFICAR LA TOKEN. PEDIRLE A LAU ALGUN METODO PARA ESTO
+
+  // if (!user || user.role !== 'admin') {
+  //   redirect('/');
+  // }
+
     return (
     <html lang="es">
       <body className='h-screen w-screen p-20 bg-gradient-to-br from-indigo-950 to-black text-white'>
