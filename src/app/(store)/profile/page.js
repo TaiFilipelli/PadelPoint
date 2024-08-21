969e65eba898dc8412286f0167bff7cec7275889
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-21 11:30:16 -0300

    new page Profile to showcase profile details and, in a future, allow to update or change profile data

---
+++ src/app/(store)/profile/page.js
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+    return (
+        <section>
+            <h1>Esto será el perfil de usuario. Se accederá mediante la navbar, en el botón renderizado nuevo.</h1>
+        </section>
+    );
+}
