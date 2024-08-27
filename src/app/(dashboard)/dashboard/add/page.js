e46ad9871b4bd6887feff58df4c783feca15fb63
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-27 12:57:37 -0300

    remake of dashboard. New button group to add multiple entities to the database. The idea is to implement some conditional rendering instead of just create a page for every single entity.

--- src/app/(dashboard)/dashboard/add/page.js
+++ src/app/(dashboard)/dashboard/add/page.js
@@ -1,7 +1,21 @@
-export default function AddPage() {
-    return (
-        <section className={``}>
+import { ButtonGroup, Button } from "@nextui-org/react";
+import { Montserrat } from "next/font/google";
 
+const mont = Montserrat({subsets:['latin'],weight:'600'});
+export default function AddEntitiesPage() {
+    return (
+        <section className="">
+            <h1 className={`${mont.className} text-3xl mb-2`}>Añadir entidades</h1>
+            <h2 className={`${mont.className} text-xl mb-8`}>Elija a continuación qué añadir a la base de datos</h2>
+            <div>
+                <ButtonGroup>
+                    <Button className="text-lg">Nuevo producto</Button>
+                    <Button className="text-lg">Nueva marca</Button>
+                    <Button className="text-lg">Nuevo rol</Button>
+                    <Button className="text-lg">Nuevo proveedor</Button>
+                    <Button className="text-lg">Nuevo tipo de producto</Button>
+                </ButtonGroup>
+            </div>
         </section>
     );
 }
