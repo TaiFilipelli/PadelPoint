bbd332509949ea900a2f39e5d6da9831353d5f3f
TaiFilipelli <taielfilipelli@gmail.com>
2024-08-30 18:19:48 -0300

    some logic to think

--- src/components/DeleteEntity.jsx
+++ src/components/DeleteEntity.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { Button, Dropdown, DropdownTrigger, DropdownItem, DropdownMenu } from "@nextui-org/react";
 
 const DeleteEntity = ({entity}) => {
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    switch(entity){
+      case 'producto':
+        console.log(`esto borrará el ${entity}`)
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <section className='flex items-center flex-col'>
+      <form onSubmit={handleSubmit}>
+        <Button type="submit" className='bg-red-600 text-white'>Eliminar {entity}</Button>
+      </form>
+    </section>
   )
 }
 
