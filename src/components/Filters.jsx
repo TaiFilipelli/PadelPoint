19be42f7215624fa57a5ee601a40e02c3ec999e9
TaiFilipelli <taielfilipelli@gmail.com>
2024-09-03 10:00:43 -0300

    finished the Products responsive and erased a single line on Footer that was making mess

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -46,11 +46,12 @@ export const Filters = () => {
     }, []);
 
   return (
-    <section className={`w-full h-20 max-[1545px]:h-28 max-[1464px]: mb-10 bg-white text-black border-1 border-black shadow-sm shadow-slate-700 rounded-lg flex flex-row max-[1000px]:flex-wrap max-[1000px]:h-full gap-10 max-[1100px]:gap-2 items-center justify-center p-2 ${pop.className}`}>
-      <h3 className='text-bold text-2xl ml-2 max-[1220px]:text-xl'>Filtrar por:</h3>
+    <section className={`w-full h-24 max-[1464px]: mb-10 bg-white text-black border-1 border-black shadow-sm shadow-slate-700 rounded-lg flex flex-row 
+                        max-[1000px]:flex-col max-[1000px]:items-center max-[1000px]:h-full gap-10 max-[1100px]:gap-2 items-center justify-center p-2 ${pop.className}`}>
+      <h3 className='text-bold text-2xl ml-2'>Filtrar por:</h3>
       <Dropdown>
             <DropdownTrigger>
-                <Button className="ml-4 p-6 text-xl max-[1200px]:text-lg bg-default-200" variant="light" radius="sm">Tipos</Button>
+                <Button className="ml-4 p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[400px]:w-2/3" variant="light" radius="sm">Tipos</Button>
             </DropdownTrigger>
             <DropdownMenu className="p-0 w-full" itemClasses={{ base: "gap-4" }}>
                 {types.map(type => (
@@ -59,22 +60,45 @@ export const Filters = () => {
             </DropdownMenu>
         </Dropdown>
         <Divider orientation='vertical'/>
-        <Slider 
-            label="Rango de precio"
-            color='foreground'
-            step={25} 
-            minValue={0} 
-            maxValue={400} 
-            defaultValue={[0,100]} 
-            formatOptions={{style: "currency", currency: "USD"}}
-            className="max-w-md max-[1530px]:w-1/5"
-            onChange={handleValueRangeChange}
-        />
+        <Dropdown className='w-full'>
+            <DropdownTrigger>
+                <Button className="ml-4 p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[400px]:w-2/3" variant="light" radius="sm">Precios</Button>
+            </DropdownTrigger>
+            <DropdownMenu className="p-2 rounded-lg">
+               <DropdownItem className='text-black'>
+                <Slider 
+                    label="USD$"
+                    color='foreground'
+                    step={25} 
+                    minValue={0} 
+                    maxValue={400} 
+                    defaultValue={[0,100]} 
+                    formatOptions={{style: "currency", currency: "USD"}}
+                    className="max-w-full"
+                    onChange={handleValueRangeChange}
+                />
+               </DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
         <Divider orientation='vertical'/>
-        <Input type='text' labelPlacement='outside' label='Buscar producto por nombre' placeholder='Buscar...' className='w-1/6 mb-1' isClearable value={searchedName} onChange={handleNameChange}/>
+        <Dropdown className='w-full'>
+            <DropdownTrigger>
+                <Button className="ml-4 p-6 text-xl max-[1200px]:text-lg bg-default-200 w-1/5 max-[880px]:w-1/3 max-[400px]:w-2/3" variant="light" radius="sm">Nombre</Button>
+            </DropdownTrigger>
+            <DropdownMenu className="p-2 rounded-lg">
+               <DropdownItem className='text-black'>
+                <Input type='text' labelPlacement='outside' label='Buscar producto por nombre' 
+                    placeholder='Buscar...' className='w-full mb-1' isClearable value={searchedName} onChange={handleNameChange}/>
+               </DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
         <Divider orientation='vertical'/>
-        <Button onClick={applyFilters} className='rounded-lg bg-black text-white mr-4 max-[1275px]:mr-0 max-[1464px]:w-1/6 max-[1325px]:w-1/5 max-[1200px]:w-1/4 max-[1100px]:w-1/3'>Aplicar filtros</Button>
-        <Button onClick={deleteFilters} className="bg-red-500 text-white rounded-lg max-[1464px]:w-1/6 max-[1325px]:w-1/5 max-[1200px]:w-1/4 max-[1100px]:w-1/3">Borrar filtros</Button>
+        <div className='flex flex-row items-center justify-center w-1/4 gap-4 max-[910px]:w-1/2 max-[500px]:w-full max-[310px]:gap-1'>
+        <Button onClick={applyFilters} 
+            className='bg-black text-white text-medium max-[1000px]:text-sm w-1/2 rounded-lg'>Aplicar filtros</Button>
+        <Button onClick={deleteFilters} 
+            className="bg-red-500 text-white text-medium max-[1000px]:text-sm w-1/2 rounded-lg">Borrar filtros</Button>
+        </div>
     </section>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       </div>
       <div className='w-1/4 h-1/2 p-5'>
         <h3 className='text-gray-200 mb-2 mt-4 font-semibold text-lg'>Aquí iran otros items a discutir!</h3>
-        <a href='https://store.steampowered.com' target='_blank' className='underline'>Link a Steam porque si xd</a>
+        <a href='https://store.steampowered.com' target='_blank' className='underline'>Steam (porque si xd)</a>
       </div>
     </section>
   )
