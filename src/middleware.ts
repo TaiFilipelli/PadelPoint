1beb313c0997c590e0e0be98673e0a1ca9f8a184
TaiFilipelli <taielfilipelli@gmail.com>
2024-09-26 13:18:40 -0300

    temporal disable of middleware to work properly on dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,33 +9,33 @@ interface CustomJwtPayload extends JwtPayload {
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-
-  if (pathname.startsWith('/dashboard')) {
-    const token = req.cookies.get('user')?.value;
-
-    if (!token) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-
-    try {
-      const decodedToken = jwtDecode<CustomJwtPayload>(token);
-
-      const roles = decodedToken.roles || [];
-      const isAdmin = roles.some((role) => role.name === 'admin');
-
-      if (!isAdmin) {
-        return NextResponse.redirect(new URL('/', req.url));
-      }
-
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-  }
-
-  return NextResponse.next();
 }
+//   if (pathname.startsWith('/dashboard')) {
+//     const token = req.cookies.get('user')?.value;
 
-export const config = {
-  matcher: ['/dashboard/:path*'],
-};
+//     if (!token) {
+//       return NextResponse.redirect(new URL('/', req.url));
+//     }
+
+//     try {
+//       const decodedToken = jwtDecode<CustomJwtPayload>(token);
+
+//       const roles = decodedToken.roles || [];
+//       const isAdmin = roles.some((role) => role.name === 'admin');
+
+//       if (!isAdmin) {
+//         return NextResponse.redirect(new URL('/', req.url));
+//       }
+
+//       return NextResponse.next();
+//     } catch (error) {
+//       return NextResponse.redirect(new URL('/', req.url));
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ['/dashboard/:path*'],
+// };
