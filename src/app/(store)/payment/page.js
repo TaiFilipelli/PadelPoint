ce5f6c5436e6efb5eb36f1ad4a45c7e017b8e451
TaiFilipelli <taielfilipelli@gmail.com>
2024-10-22 18:23:24 -0300

    new route and page MP_Page to handle payments with Mercado Pago

---
+++ src/app/(store)/payment/page.js
@@ -0,0 +1,7 @@
+export default function MP_Page() {
+    return (
+        <section className="h-[60vh] flex flex-col items-center p-16 px-auto">
+            <h1 className="my-4 text-black font-bold">Mercado Pago Payment</h1>
+        </section>
+    );
+}
