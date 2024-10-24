9906db2303b3330f1179af0ebb28aa96d74e4e76
TaiFilipelli <taielfilipelli@gmail.com>
2024-10-24 16:14:30 -0300

    new routes to test back_url's in MP API

---
+++ src/app/(store)/payment/failure/page.js
@@ -0,0 +1,7 @@
+export default function FailurePage() {
+    return (
+        <section className="h-[60vh] flex flex-col items-center p-16 px-auto">
+            <h1 className="my-4 font-bold text-black">pago fallido...y encima nos comimos 3.</h1>  
+        </section>
+    );
+}
---
+++ src/app/(store)/payment/pending/page.js
@@ -0,0 +1,7 @@
+export default function PendingPage() {
+    return (
+        <section className="h-[60vh] flex flex-col items-center p-16 px-auto">
+            <h1 className="my-4 font-bold text-black">Pago pendiente...</h1>
+        </section>
+    );
+}
