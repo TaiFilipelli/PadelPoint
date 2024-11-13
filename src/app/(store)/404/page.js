2831fa952f1dff6dde595a4a7dd7beab4a65d306
TaiFilipelli <taielfilipelli@gmail.com>
2024-11-13 10:19:18 -0300

    more responsive changes

--- src/app/(store)/404/page.js
+++ src/app/(store)/404/page.js
@@ -6,11 +6,11 @@ const popDesc = Poppins({subsets:['latin'], weight:'300'})
 
 export default function Page404() {
     return (
-        <section className="flex flex-col items-center justify-center text-center p-20 bg-[#264492]">
-            <img src="/404.png" className="h-[21rem]"/>
-            <h1 className={`${popTitle.className} text-5xl`}>Error 404: Not Found</h1>
-            <h2 className={`${popDesc.className} text-2xl mb-4`}>No se ha encontrado la página que busca</h2>
-            <Link href='/' className={`${popDesc.className} text-xl hover:underline`}>Volver al inicio</Link>
+        <section className="flex flex-col items-center justify-center text-center p-20 max-[400px]:p-10 bg-[#264492]">
+            <img src="/404.png" className="h-[21rem] max-[500px]:h-auto"/>
+            <h1 className={`${popTitle.className} text-5xl max-[400px]:text-3xl mb-4`}>Error 404: Not Found</h1>
+            <h2 className={`${popDesc.className} text-2xl max-[400px]:text-xl mb-8`}>No se ha encontrado la página que busca</h2>
+            <Link href='/' className={`${popDesc.className} text-xl underline`}>Volver al inicio</Link>
         </section>
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
   return (
     <section className='w-full bg-gray-600 text-white flex flex-row justify-center gap-7 bottom-0'>
       <div className='w-1/2 h-1/2 p-5 flex flex-col'>
-        <h3 className='text-2xl mb-2 mt-4 font-semibold'>Conocé más de nosotros</h3>
+        <h3 className='text-2xl max-[400px]:text-xl mb-2 mt-4 font-semibold'>Conocé más de nosotros</h3>
         <Link href='/about' className='underline text-lg mb-4'>¿Quiénes somos?</Link>
         <Link href='/about#faq' className='underline text-lg'>FAQ</Link>
       </div>
       <div className='w-1/2 h-1/2 p-5'>
-        <h3 className='text-2xl mb-2 mt-4 font-semibold'>Seguinos!</h3>
+        <h3 className='text-2xl max-[400px]:text-xl mb-2 mt-4 font-semibold'>Seguinos!</h3>
         <a href='https://www.instagram.com/_padelpoint/' target='_blank' className='underline text-lg'>Instagram</a>
       </div>
     </section>
