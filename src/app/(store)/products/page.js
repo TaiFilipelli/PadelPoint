4221adba09c9d2b12379ec403422ceec72e250a7
TaiFilipelli <taielfilipelli@gmail.com>
2024-12-12 11:20:48 -0300

    AMAZING reestructuration of the whole Products page: logic of filters has also been changed to improve performance and UX. Beautiful changes, i'm going to cry right now

--- src/app/(store)/products/page.js
+++ src/app/(store)/products/page.js
@@ -5,14 +5,23 @@ import { useEffect, useState } from "react";
 import { getProducts} from "../../../data/storeData";
 import Filters from "../../../components/Filters";
 import { SmileySad } from "@phosphor-icons/react";
+import { type } from "os";
 
 const pop = Poppins({subsets:["latin"], weight:'600'})
 export default function ProductsList() {
     const [products, setProducts] = useState([]);
+    const [filters, setFilters] = useState({
+        brand: '',
+        name: '',
+        minPrice: 1,
+        maxPrice: 1000000,
+        type: ''
+    });
     const [isLoading, setIsLoading] = useState(true);
-    const dataProducts = async (params) => {
+
+    const dataProducts = async () => {
         try {
-            const data = await getProducts(params);
+            const data = await getProducts(filters);
             setProducts(data.recourse);
             setIsLoading(false);
         } catch (error) {
@@ -22,28 +31,20 @@ export default function ProductsList() {
     };
 
     useEffect(() => {
-        const brand = localStorage.getItem('selectedBrand');
-        const name = localStorage.getItem('name');
-        const minPrice = localStorage.getItem('minPrice');
-        const maxPrice = localStorage.getItem('maxPrice');
-        const type = localStorage.getItem('type');
-        const params = {};
+        dataProducts();
+    }, [filters]);
 
-        if (brand !== null) params.brand = brand;
-        if (name !== null && name!=='') params.name = name;
-        if (minPrice !== null) {
-            const minPriceNumber = Number(minPrice);
-            params.minPrice = minPriceNumber === 0 ? 1 : minPriceNumber;
-        }
-        if (maxPrice !== null) params.maxPrice = maxPrice;
-        if(type!==null) params.type = type;
-        dataProducts(params);
-    }, []);
+    const handleFilterChange = (newFilters) => {
+        setFilters((prevFilters) => ({
+            ...prevFilters,
+            ...newFilters,
+        }));
+    }
 
     return (
         <section className="flex justify-center items-center text-center flex-col py-10 px-5 mt-6 bg-[#264492]">
             <h1 className={`${pop.className} text-6xl mb-10 max-[390px]:text-4xl`}>Productos</h1>
-            <Filters/>
+            <Filters filters={filters} onFilterChange={handleFilterChange}/>
             <section className="w-4/5 max-w-[4/5] max-[1260px]:w-full">
                 <div className="flex flex-wrap justify-center max-[600px]:pt-0 gap-3">
                     {isLoading ? (
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,125 +3,155 @@ import { useEffect, useState } from 'react';
 import { Poppins } from 'next/font/google';
 import { getTypes, getBrands } from '../data/storeData';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, Slider, Divider, Input } from '@nextui-org/react';
-import {trackSearch} from '../../utils/pixel';
+import { trackSearch } from '../../utils/pixel';
 
-const pop = Poppins({subsets:['latin'], weight:'500'})
+const pop = Poppins({ subsets: ['latin'], weight: '500' });
 
-export const Filters = () => {
-    const [searchedName, setSearchedName] = useState('');
-    const [priceRange, setPriceRange] = useState([0,1000000]);
-    const [types, setTypes] = useState([])
-    const [brands, setBrands] = useState([])
-    const [selectedType, setSelectedType] = useState('');
-    const [selectedBrand, setSelectedBrand] = useState('');
+export const Filters = ({ filters, onFilterChange }) => {
+    const [types, setTypes] = useState([]);
+    const [brands, setBrands] = useState([]);
 
-    const deleteFilters = () =>{
-        localStorage.removeItem('selectedBrand');
-        localStorage.removeItem('name');
-        localStorage.removeItem('minPrice');
-        localStorage.removeItem('maxPrice');
-        localStorage.removeItem('type');
-        window.location.reload();
-    }
-    const applyFilters = () =>{
-        localStorage.setItem('name',searchedName);
-        localStorage.setItem('minPrice', priceRange[0]);
-        localStorage.setItem('maxPrice', priceRange[1]);
-        localStorage.setItem('type',selectedType);
-        localStorage.setItem('selectedBrand',selectedBrand);
-        window.location.reload();
-    }
-    const fetchData = async() =>{
-        const typesP = await getTypes();
-        const brandsP = await getBrands();
-        setBrands(brandsP.recourse);
-        setTypes(typesP.recourse);
-    }
-    const handleNameChange = (event) => {
-        setSearchedName(event.target.value);
-        trackSearch(event.target.value);
-    }
-    const handleValueRangeChange = (value) => {
-        setPriceRange(value);
-    }
-    const handleTypeSelect = (value) => {
-        setSelectedType(value);
-    }
-    const handleBrandSelect = (value) => {
-        setSelectedBrand(value);
-    }
-
-    useEffect(()=>{
+    useEffect(() => {
+        const fetchData = async () => {
+            const typesData = await getTypes();
+            const brandsData = await getBrands();
+            setTypes(typesData.recourse);
+            setBrands(brandsData.recourse);
+        };
         fetchData();
     }, []);
 
-  return (
-    <section className={`w-full h-24 max-[1464px]: mb-10 bg-white text-black border-1 border-black shadow-sm shadow-slate-700 rounded-lg flex flex-row 
-    max-[1000px]:flex-col max-[1000px]:items-center max-[1000px]:h-full gap-10 max-[1100px]:gap-2 items-center justify-center p-2 ${pop.className}`}>
-      <h3 className='text-bold text-2xl ml-2'>Filtrar por:</h3>
-      <Dropdown>
-            <DropdownTrigger>
-                <Button className="p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm"> {selectedType? selectedType : 'Tipos'}</Button> 
-            </DropdownTrigger>
-            <DropdownMenu className="p-0 w-full" itemClasses={{ base: "gap-4" }}>
-                {types.map(type => (
-                  <DropdownItem key={type.id} onClick={() => handleTypeSelect(type.name)} className='text-black'>{type.name}</DropdownItem>
-                ))}
-            </DropdownMenu>
-        </Dropdown>
-        <Divider orientation='vertical'/>
-        <Dropdown className='w-full'>
-            <DropdownTrigger>
-                <Button className="p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm">Precios</Button>
-            </DropdownTrigger>
-            <DropdownMenu className="p-2 rounded-lg" >
-               <DropdownItem className='text-black' isReadOnly>
-                <Slider
-                    label='ARS'
-                    color='foreground'
-                    step={25} 
-                    minValue={0} 
-                    maxValue={500000} 
-                    defaultValue={[0,500000]} 
-                    formatOptions={{style: "currency", currency: "ARS"}}
-                    className="max-w-full"
-                    onChange={handleValueRangeChange}
-                />
-               </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
-        <Divider orientation='vertical'/>
-        <Dropdown className='w-full'>
-            <DropdownTrigger>
-                <Button className="p-6 text-xl max-[1200px]:text-lg bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm">Modelo</Button>
-            </DropdownTrigger>
-            <DropdownMenu className="p-2 rounded-lg">
-               <DropdownItem className='text-black' isReadOnly>
-                <Input type='text' labelPlacement='outside' label='Buscar producto por nombre' 
-                    placeholder='Buscar...' className='w-full mb-1' isClearable value={searchedName} onChange={handleNameChange}/>
-               </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
-        <Divider orientation='vertical'/>
-        <Dropdown className='w-full'>
-            <DropdownTrigger>
-                <Button className="p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm">Marcas</Button>
-            </DropdownTrigger>
-            <DropdownMenu className="p-2 rounded-lg" >
-            {brands.map(brand => (
-                  <DropdownItem key={brand.id} onClick={() => handleBrandSelect(brand.name)} className='text-black'>{brand.name}</DropdownItem>
-                ))}
-            </DropdownMenu>
-        </Dropdown>
-        <Divider orientation='vertical'/>
-        <div className='flex flex-row items-center justify-center w-1/4 gap-4 max-[910px]:w-1/2 max-[500px]:w-full max-[310px]:gap-1'>
-        <Button onClick={applyFilters} 
-            className='bg-black text-white text-medium max-[1000px]:text-sm w-1/2 rounded-lg'>Aplicar filtros</Button>
-        <Button onClick={deleteFilters} 
-            className="bg-red-500 text-white text-medium max-[1000px]:text-sm w-1/2 rounded-lg">Borrar filtros</Button>
-        </div>
-    </section>
-  )
-}
+    const handleNameChange = (event) => {
+        const value = event.target.value;
+        onFilterChange({ name: value });
+        trackSearch(value);
+    };
 
-export default Filters
+    const handlePriceRangeChange = (value) => {
+        onFilterChange({ minPrice: value[0], maxPrice: value[1] });
+    };
+
+    const handleTypeSelect = (value) => {
+        onFilterChange({ type: value });
+    };
+
+    const handleBrandSelect = (value) => {
+        onFilterChange({ brand: value });
+    };
+
+    const clearFilters = () => {
+        onFilterChange({
+            brand: '',
+            name: '',
+            minPrice: 1,
+            maxPrice: 1000000,
+            type: '',
+        });
+    };
+
+    return (
+        <section
+            className={`w-full h-24 max-[1464px]:mb-10 bg-white text-black border-1 border-black shadow-sm shadow-slate-700 rounded-lg flex flex-row 
+            max-[1000px]:flex-col max-[1000px]:items-center max-[1000px]:h-full gap-10 max-[1100px]:gap-2 items-center justify-center p-2 ${pop.className}`}
+        >
+            <h3 className="text-bold text-2xl ml-2">Filtrar por:</h3>
+
+            <Dropdown>
+                <DropdownTrigger>
+                    <Button className="p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm">
+                        {filters.type || 'Tipos'}
+                    </Button>
+                </DropdownTrigger>
+                <DropdownMenu className="p-0 w-full">
+                    {types.map((type) => (
+                        <DropdownItem
+                            key={type.id}
+                            onClick={() => handleTypeSelect(type.name)}
+                            className="text-black"
+                        >
+                            {type.name}
+                        </DropdownItem>
+                    ))}
+                </DropdownMenu>
+            </Dropdown>
+
+            <Divider orientation="vertical" />
+
+            <Dropdown className="w-full">
+                <DropdownTrigger>
+                    <Button className="p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm">
+                        Precios
+                    </Button>
+                </DropdownTrigger>
+                <DropdownMenu className="p-2 rounded-lg">
+                    <DropdownItem className="text-black" isReadOnly>
+                        <Slider
+                            label="ARS"
+                            color="foreground"
+                            step={25}
+                            minValue={0}
+                            maxValue={1000000}
+                            value={[filters.minPrice, filters.maxPrice]}
+                            formatOptions={{ style: 'currency', currency: 'ARS' }}
+                            className="max-w-full"
+                            onChange={handlePriceRangeChange}
+                        />
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+
+            <Divider orientation="vertical" />
+
+            <Dropdown className="w-full">
+                <DropdownTrigger>
+                    <Button className="p-6 text-xl max-[1200px]:text-lg bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm">
+                        Modelo
+                    </Button>
+                </DropdownTrigger>
+                <DropdownMenu className="p-2 rounded-lg">
+                    <DropdownItem className="text-black" isReadOnly>
+                        <Input
+                            type="text"
+                            labelPlacement="outside"
+                            label="Buscar producto por nombre"
+                            placeholder="Buscar..."
+                            className="w-full mb-1"
+                            isClearable
+                            value={filters.name}
+                            onChange={handleNameChange}
+                        />
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+
+            <Divider orientation="vertical" />
+
+            <Dropdown className="w-full">
+                <DropdownTrigger>
+                    <Button className="p-6 text-xl bg-default-200 w-1/5 max-[880px]:w-1/3 max-[600px]:w-2/3" variant="light" radius="sm">
+                        {filters.brand || 'Marcas'}
+                    </Button>
+                </DropdownTrigger>
+                <DropdownMenu className="p-2 rounded-lg">
+                    {brands.map((brand) => (
+                        <DropdownItem
+                            key={brand.id}
+                            onClick={() => handleBrandSelect(brand.name)}
+                            className="text-black"
+                        >
+                            {brand.name}
+                        </DropdownItem>
+                    ))}
+                </DropdownMenu>
+            </Dropdown>
+            <Divider orientation="vertical" />
+            <div className="flex flex-row items-center justify-center w-1/4 gap-4 max-[910px]:w-1/2 max-[500px]:w-full max-[310px]:gap-1">
+                <Button onClick={clearFilters} className="bg-red-500 text-white text-medium max-[1000px]:text-sm w-full rounded-lg">
+                    Borrar filtros
+                </Button>
+            </div>
+        </section>
+    );
+};
+
+export default Filters;
