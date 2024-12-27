fab53cbe4b1f06d5c2f43a4585a8c72fe607c381
TaiFilipelli <taielfilipelli@gmail.com>
2024-12-27 12:33:54 -0300

    minor change with the images on production just in case

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
 
     removeConsole: process.env.NODE_ENV === "PROD",
 
+    images:{
+        unoptimized: true,
+    },
+
     experimental:{
         missingSuspenseWithCSRBailout: false,
     }
