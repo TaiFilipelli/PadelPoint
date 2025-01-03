8be5d37c3c4b21f539734d68657da9b5e8b70d0a
TaiFilipelli <taielfilipelli@gmail.com>
2025-01-03 13:08:15 -0300

    depuration

--- src/app/UserContext.js
+++ src/app/UserContext.js
@@ -16,13 +16,17 @@ export const UserProvider = ({ children }) => {
       const userData = await checkUserState();
       console.log('Chequeate la data de user que viene desde acá:', userData);
       if (userData.isLogged===true || userData.refreshTokenExists===true) {
+        console.log('Está bien, el usuario está logeado o tiene un token de refresco');
         setUser({
           isLogged: true,
           isAdmin: userData.payload.roles.some(role => role.name === 'admin'),
           username: localStorage.getItem('username'),
         });
+        console.log('El usuario está bien, se ha inicializado correctamente: Está loggeado?', user.isLogged,'; es admin? ', user.isAdmin,'; su nombre es ',user.username);
       } else {
+        console.log('El usuario no está loggeado: eso o no se toma el token');
         setUser({ isLogged: false, isAdmin: false, username: '' });
+        console.log('Se borró el usuario y ahora se borra de localStorage el nombre de usuario');
         localStorage.removeItem('username');
       }
     } catch (err) {
@@ -34,9 +38,13 @@ export const UserProvider = ({ children }) => {
 
   useEffect(() => {
     if (user.isLogged===false){
+      console.log('El usuario no está logeado, se borra el usuario y se borra de localStorage el nombre de usuario');
       setUser({username:''});
       localStorage.removeItem('username');
+      console.log('Se inicializa el usuario acá ');
       initializeUser();
+    }else{
+      return user;
     }
   }, []);
 
