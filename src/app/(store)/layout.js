7ebcd7495a3c3317ff71862a50f251590c2b63c1
TaiFilipelli <taielfilipelli@gmail.com>
2025-01-02 22:53:29 -0300

    added new global layout to put the User context on it, testing auth.

--- src/app/(store)/layout.js
+++ src/app/(store)/layout.js
@@ -1,6 +1,5 @@
 import '../../../styles/globals.css'
 import { Providers } from '../providers';
-import {UserProvider} from '../UserContext';
 import Nav from '../../components/store/Navbar'
 import AnimatedBar from '../../components/store/AnimatedBar'
 import Footer from '../../components/store/Footer';
@@ -15,16 +14,14 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="es">
-      <body className='h-screen bg-transparent text-white dark:bg-black dark:text-white pt-36 max-[700px]:pt-28'>
-        <UserProvider>
+      <main className='h-screen bg-transparent text-white dark:bg-black dark:text-white pt-36 max-[700px]:pt-28'>
           <Providers>
             <AnimatedBar/>
             <Nav/>
             {children}
             <Footer />
           </Providers>
-        </UserProvider>
-      </body>
+      </main>
     </html>
   );
 }
---
+++ src/app/layout.js
@@ -0,0 +1,11 @@
+import { UserProvider } from './UserContext';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>
+        <UserProvider>{children}</UserProvider>
+      </body>
+    </html>
+  );
+}
